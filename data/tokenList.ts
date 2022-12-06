2adaa5dc3eabbdfea6c73d08b588644e07e0041c
Tristan | Tribot17 <tribot17@gmail.com>
2022-12-07 00:29:12 +0100

    add tokenList

---
+++ data/tokenList.ts
@@ -0,0 +1,21 @@
+export const tokenList = [
+  {
+    name: "Ether",
+    address: "",
+    symbol: "ETH",
+    logo: "/images/eth_token_logo.png",
+  },
+  {
+    name: "Wrapped Ether",
+    address: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
+    symbol: "WETH",
+    logo: "/images/weth_token_logo.png",
+
+  },
+  {
+    name: "Uniswap",
+    address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+    symbol: "UNI",
+    logo: "/images/uni_token_logo.png",
+  },
+];

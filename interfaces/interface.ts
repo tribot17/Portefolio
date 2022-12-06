503b2623543c307cc74fd2a287335112ce7d3f02
Tristan | Tribot17 <tribot17@gmail.com>
2022-12-07 00:28:04 +0100

    add token interface

---
+++ interfaces/interface.ts
@@ -0,0 +1,14 @@
+export interface experience {
+  title: string;
+  compagny: string;
+  time: string;
+  description: string;
+  skills: object[];
+}
+
+export interface token {
+  name: string;
+  address: string;
+  symbol: string;
+  logo: string;
+}
